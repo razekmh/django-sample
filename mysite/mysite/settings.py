c17a26ef59d3781c6dd8133eec2b6b63963bedc0
mahmoud <razekmh@gmail.com>
2021-11-05 15:46:58 +0000

    fix key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8wc=l*oxu!8y8hp7lu1gx$9#nb5-4ga-xtw^wq&%%#t#gla5$r'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -123,3 +122,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+        from .settings_local import *
+except ImportError:
+        pass
+
